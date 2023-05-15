a76fe4f51841c61558ba30d0a977b1923090524b
Sergey <e-box@live.ru>
2023-05-15 20:23:41 +0800

    finished Item class and tests for this class, homework1 done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_instance():
+    return Item('milk', 59.5, 100)
+
+
+def test_item_params(item_instance):
+    assert item_instance.price == 59.5
+    assert item_instance.quantity == 100
+
+
+def test_calculate_price(item_instance):
+    assert item_instance.calculate_total_price() == 5950
+
+
+def test_apply_discount(item_instance):
+    item_instance.apply_discount()
+    assert item_instance.price == 59.5
+    Item.pay_rate = 0.5
+    item_instance.apply_discount()
+    assert item_instance.price == 59.5 * 0.5
+
+
+
